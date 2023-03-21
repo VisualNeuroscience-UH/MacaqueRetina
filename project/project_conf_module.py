47f7447759b45a98643c82e07d6b0a6cb61c36a0
Simo Vanni <simo.vanni@helsinki.fi>
2023-03-21 15:13:20 +0200

    minor

--- project/project_conf_module.py
+++ project/project_conf_module.py
@@ -114,7 +114,7 @@ my_retina = {
     "model_density": 1.0,
     "randomize_position": 0.05,
     "stimulus_center": 4.45 + 0j,
-    "model_type": "VAE",  # "FIT", "VAE" for variational autoencoder, or GAN for generative adversarial network.
+    "model_type": "FIT",  # "FIT", "VAE" for variational autoencoder, or GAN for generative adversarial network.
 }
 
 
@@ -318,9 +318,9 @@ if __name__ == "__main__":
 
     # options are defined in my_retina_options
 
-    PM.construct_retina.build()
+    # PM.construct_retina.build()
 
-    PM.construct_retina.save_mosaic()
+    # PM.construct_retina.save_mosaic()
 
     # PM.construct_retina.show_build_process(show_all_spatial_fits=True)
 
@@ -370,7 +370,7 @@ if __name__ == "__main__":
 
     # # # If you want to load with object, it is possible by:
     # PM.working_retina.load_stimulus(PM.stimulate)
-    # PM.working_retina.load_stimulus()
+    PM.working_retina.load_stimulus()
 
     # movie = vs.NaturalMovie(r'C:\Users\Simo\Laskenta\Stimuli\videoita\naturevids\nature1.avi', fps=100, pix_per_deg=60)# => METADATA
     # ret.load_stimulus(movie)# => METADATA
@@ -379,19 +379,19 @@ if __name__ == "__main__":
     ### Show single ganglion cell response ###
     #################################
 
-    # example_gc = 2  # int or 'None'
-    # PM.working_retina.convolve_stimulus(example_gc)
+    example_gc = 2  # int or 'None'
+    PM.working_retina.convolve_stimulus(example_gc)
 
-    # PM.viz.show_spatiotemporal_filter(PM.working_retina)
-    # PM.viz.show_convolved_stimulus(PM.working_retina)
+    PM.viz.show_spatiotemporal_filter(PM.working_retina)
+    PM.viz.show_convolved_stimulus(PM.working_retina)
 
     #################################
     ### Run multiple trials for single cell ###
     #################################
 
-    # PM.working_retina.run_with_my_run_options()
+    PM.working_retina.run_with_my_run_options()
 
-    # PM.viz.show_gc_responses(PM.working_retina)
+    PM.viz.show_gc_responses(PM.working_retina)
 
     # PM.viz.show_stimulus_with_gcs(PM.working_retina, example_gc=my_run_options["cell_index"], frame_number=51)
 
