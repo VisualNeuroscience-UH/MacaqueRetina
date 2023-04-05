6ec970d218314d341e8e5c30afad97d979a5aabc
Simo Vanni <simo.vanni@helsinki.fi>
2023-04-05 16:10:40 +0300

    countlines had become dysfunc, updated to work

--- project/project_utilities_module.py
+++ project/project_utilities_module.py
@@ -10,55 +10,55 @@ import numpy as np
 import pandas as pd
 
 
-
-
-class ProjectUtilities():
-    '''
+class ProjectUtilities:
+    """
     Utilities for ProjectManager class. This class is not instantiated. It serves as a container for project independent helper functions.
-    '''
-    
+    """
+
     def round_to_n_significant(self, value_in, significant_digits=3):
 
         boolean_test = value_in != 0
 
         if boolean_test and not np.isnan(value_in):
             int_to_subtract = significant_digits - 1
-            value_out = round(value_in, -int(np.floor(np.log10(np.abs(value_in))) - int_to_subtract))
+            value_out = round(
+                value_in, -int(np.floor(np.log10(np.abs(value_in))) - int_to_subtract)
+            )
         else:
             value_out = value_in
-            
-        return value_out    
+
+        return value_out
 
     def destroy_from_folders(self, path=None, dict_key_list=None):
-        '''
+        """
         Run destroy_data from root folder, deleting selected variables from data files one level towards leafs.
-        '''
+        """
 
         if path is None:
-            p = Path('.')
+            p = Path(".")
         elif isinstance(path, Path):
             p = path
         elif isinstance(path, str):
             p = Path(path)
             if not p.is_dir():
-                raise ArgumentError(f'path argument is not valid path, aborting...')
+                raise ArgumentError(f"path argument is not valid path, aborting...")
 
         folders = [x for x in p.iterdir() if x.is_dir()]
         metadata_full = []
         for this_folder in folders:
             for this_file in list(this_folder.iterdir()):
-                if 'metadata' in str(this_file):
+                if "metadata" in str(this_file):
                     metadata_full.append(this_file.resolve())
 
         for this_metadata in metadata_full:
             try:
-                print(f'Updating {this_metadata}')
+                print(f"Updating {this_metadata}")
                 updated_meta_full, foo_df = self.update_metadata(this_metadata)
                 self.destroy_data(updated_meta_full, dict_key_list=dict_key_list)
             except FileNotFoundError:
-                print(f'No files for {this_metadata}, nothing changed...')
+                print(f"No files for {this_metadata}, nothing changed...")
 
-    def destroy_data(self, meta_fname, dict_key_list=None ):
+    def destroy_data(self, meta_fname, dict_key_list=None):
         """
         Sometimes you have recorded too much and you want to reduce the filesize by removing some data.
 
@@ -71,12 +71,16 @@ class ProjectUtilities():
         Currently specific to destroying the second level of keys, as in above example.
         """
         if dict_key_list is None:
-            raise ArgumentError(dict_key_list, 'dict_key_list is None - nothing to do, aborting...')
-        
-        if Path(meta_fname).is_file() and 'metadata' in str(meta_fname):
+            raise ArgumentError(
+                dict_key_list, "dict_key_list is None - nothing to do, aborting..."
+            )
+
+        if Path(meta_fname).is_file() and "metadata" in str(meta_fname):
             meta_df = self.data_io.get_data(meta_fname)
         else:
-            raise FileNotFoundError('The first argument must be valid metadata file name in current folder, or full path to metadata file')
+            raise FileNotFoundError(
+                "The first argument must be valid metadata file name in current folder, or full path to metadata file"
+            )
 
         def format(filename, dict_key_list):
             # This will destroy the selected data
@@ -86,16 +90,25 @@ class ProjectUtilities():
                     try:
                         del data_dict[key][key2]
                     except KeyError:
-                        print(f'Key {key2} not found, assuming removed, nothing changed...')
+                        print(
+                            f"Key {key2} not found, assuming removed, nothing changed..."
+                        )
                         return
             self.data_io.write_to_file(filename, data_dict)
 
-        for filename in meta_df['Full path']:
+        for filename in meta_df["Full path"]:
             if Path(filename).is_file():
                 format(filename, dict_key_list)
 
     def pp_df_full(self, df):
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.max_colwidth', -1): 
+        with pd.option_context(
+            "display.max_rows",
+            None,
+            "display.max_columns",
+            None,
+            "display.max_colwidth",
+            -1,
+        ):
             print(df)
 
     def end2idx(self, t_idx_end, n_samples):
@@ -106,58 +119,65 @@ class ProjectUtilities():
             t_idx_end = n_samples + t_idx_end
         return t_idx_end
 
-    def metadata_manipulator(self, meta_full=None, filename=None, multiply_rows=1, replace_dict={}):
-        '''
+    def metadata_manipulator(
+        self, meta_full=None, filename=None, multiply_rows=1, replace_dict={}
+    ):
+        """
         Replace strings in a metadata file.
         :param path: str or pathlib object
         :param filename: str, metadata filename, if empty, search most recent in path
-        :param replace_dict: dict, 
+        :param replace_dict: dict,
             keys: 'columns', 'find' and 'replace'
-            values: lists of same length  
+            values: lists of same length
             key: 'rows'
             values: list of row index values (as in df.loc) for the changes to apply
-        '''
+        """
 
         if meta_full is None:
-            raise ArgumentError('Need full path to metadatafile, aborting...')
+            raise ArgumentError("Need full path to metadatafile, aborting...")
 
         if not replace_dict:
-            raise ArgumentError('Missing replace dict, aborting...')
+            raise ArgumentError("Missing replace dict, aborting...")
 
         data_df = self.data_io.load_from_file(meta_full)
 
         # multiply rows by factor multiply_rows
         multiply_rows = 2
-        new_df = pd.DataFrame(np.repeat(data_df.values, multiply_rows, axis=0), columns = data_df.columns)
+        new_df = pd.DataFrame(
+            np.repeat(data_df.values, multiply_rows, axis=0), columns=data_df.columns
+        )
 
-        for this_row in replace_dict['rows']:
-            for col_idx, this_column in enumerate(replace_dict['columns']):
-                f = replace_dict['find'][col_idx]
-                r = replace_dict['replace'][col_idx]
-                print(f'Replacing {f=} for {r=}, for {this_row=}, {this_column=}')
-                new_df.loc[this_row][this_column] = (
-                str(new_df.loc[this_row][this_column]).replace(f,r) # str method
-                )
+        for this_row in replace_dict["rows"]:
+            for col_idx, this_column in enumerate(replace_dict["columns"]):
+                f = replace_dict["find"][col_idx]
+                r = replace_dict["replace"][col_idx]
+                print(f"Replacing {f=} for {r=}, for {this_row=}, {this_column=}")
+                new_df.loc[this_row][this_column] = str(
+                    new_df.loc[this_row][this_column]
+                ).replace(
+                    f, r
+                )  # str method
 
         self.pp_df_full(new_df)
         new_meta_full = self._write_updated_metadata_to_file(meta_full, new_df)
-        print(f'Created {new_meta_full}')
+        print(f"Created {new_meta_full}")
 
     # Debugging
     def pp_df_memory(self, df):
         BYTES_TO_MB_DIV = 0.000001
-        mem = round(df.memory_usage().sum() * BYTES_TO_MB_DIV, 3) 
+        mem = round(df.memory_usage().sum() * BYTES_TO_MB_DIV, 3)
         print("Memory usage is " + str(mem) + " MB")
 
     def pp_obj_size(self, obj):
-        from IPython.lib.pretty import pprint 
+        from IPython.lib.pretty import pprint
+
         pprint(obj)
-        print(f'\nObject size is {sys.getsizeof(obj)} bytes')
+        print(f"\nObject size is {sys.getsizeof(obj)} bytes")
 
     def get_added_attributes(self, obj1, obj2):
 
         XOR_attributes = set(dir(obj1)).symmetric_difference(dir(obj2))
-        unique_attributes_list = [n for n in XOR_attributes if not n.startswith('_')]
+        unique_attributes_list = [n for n in XOR_attributes if not n.startswith("_")]
         return unique_attributes_list
 
     def pp_attribute_types(self, obj, attribute_list=[]):
@@ -166,33 +186,40 @@ class ProjectUtilities():
             attribute_list = dir(obj)
 
         for this_attribute in attribute_list:
-            attribute_type = eval(f'type(obj.{this_attribute})')
-            print(f'{this_attribute}: {attribute_type}')
+            attribute_type = eval(f"type(obj.{this_attribute})")
+            print(f"{this_attribute}: {attribute_type}")
 
     def countlines(self, start, lines=0, header=True, begin_start=None):
         # Counts lines in folder .py files.
         # From https://stackoverflow.com/questions/38543709/count-lines-of-code-in-directory-using-python
+
+        start = Path(start)
+
         if header:
-            print('{:>10} |{:>10} | {:<20}'.format('ADDED', 'TOTAL', 'FILE'))
-            print('{:->11}|{:->11}|{:->20}'.format('', '', ''))
+            print("{:>10} |{:>10} | {:<20}".format("ADDED", "TOTAL", "FILE"))
+            print("{:->11}|{:->11}|{:->20}".format("", "", ""))
 
         for thing in Path.iterdir(start):
             thing = Path.joinpath(start, thing)
             if thing.is_file():
-                if thing.endswith('.py'):
-                    with open(thing, 'r') as f:
+                if str(thing).endswith(".py"):
+                    with open(thing, "r") as f:
                         newlines = f.readlines()
                         newlines = len(newlines)
                         lines += newlines
 
                         if begin_start is not None:
-                            reldir_of_thing = '.' + thing.replace(begin_start, '')
+                            reldir_of_thing = "." + str(thing).replace(
+                                str(begin_start), ""
+                            )
                         else:
-                            reldir_of_thing = '.' + thing.replace(start, '')
+                            reldir_of_thing = "." + str(thing).replace(str(start), "")
 
-                        print('{:>10} |{:>10} | {:<20}'.format(
-                                newlines, lines, reldir_of_thing))
-
+                        print(
+                            "{:>10} |{:>10} | {:<20}".format(
+                                newlines, lines, reldir_of_thing
+                            )
+                        )
 
         for thing in Path.iterdir(start):
             thing = Path.joinpath(start, thing)
@@ -200,4 +227,3 @@ class ProjectUtilities():
                 lines = self.countlines(thing, lines, header=False, begin_start=start)
 
         return lines
-
