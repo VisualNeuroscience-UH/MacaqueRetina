29e163af552e126346929e4ed8ba83a5fb01c5af
simo <simo.vanni@helsinki.fi>
2024-03-26 15:04:26 +0200

    brian2cuda installation, now including compatible brian installation

--- install_ret_pt_linux.sh
+++ install_ret_pt_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install packages with pip
+pip install brian2cuda
 pip install ipython
 pip install torch torchvision torchaudio
-pip install brian2
 pip install notebook jupyterlab scikit-learn pytest scikit-image matplotlib tqdm seaborn
 pip install opencv-python-headless
 pip install colorednoise
@@ -16,10 +16,8 @@ pip install torchsummary
 pip install pyshortcuts
 pip install Shapely
 
-# Go one directory up from MacaqueRetina repo
-cd .. 
-
 # Clone cxsystem2 repo and install
+cd .. 
 git clone https://github.com/VisualNeuroscience-UH/CxSystem2.git
 cd CxSystem2
 pip install -U -e .
@@ -28,7 +26,3 @@ pip install -U -e .
 cxsystem2
 
 cd ../MacaqueRetina
-
-# Deactivate the virtual environment
-deactivate
-
